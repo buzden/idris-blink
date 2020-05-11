68b28d7ca8f960e88651083f3756fc3563c498f4
Denis Buzdalov <public@buzden.ru>
2020-05-11 16:32:20 +0300

    Ersatz-stdlib from the Idris-RTS-for-Arduino has been used.

--- Idris.mk
+++ Idris.mk
@@ -8,11 +8,14 @@ NO_CORE_MAIN_CPP := "true"
 
 RTS_OBJS = \
 	$(OBJDIR)/rts/arduino/idris_main.o \
+	$(OBJDIR)/rts/arduino/ersatz_stdlib.o \
+	$(OBJDIR)/rts/arduino/cpp_wrappers.cpp.o \
 	$(OBJDIR)/rts/idris_gc.o \
 	$(OBJDIR)/rts/idris_rts.o \
 	$(OBJDIR)/rts/idris_bitstring.o \
 	$(OBJDIR)/rts/idris_gmp.o \
 	$(OBJDIR)/rts/idris_heap.o \
+	$(OBJDIR)/rts/idris_stdfgn.o \
 	$(OBJDIR)/rts/mini-gmp.o
 
 LOCAL_C_SRCS += $(IDRIS_MAIN:.idr=.c)
@@ -27,3 +30,7 @@ include $(ARDUINO_MAKEFILE_PATH)
 $(OBJDIR)/rts/%.o: $(IDRIS_RTS_PATH)/%.c $(COMMON_DEPS) | $(OBJDIR)
 	@$(MKDIR) $(dir $@)
 	$(CC) -MMD -c $(CPPFLAGS) $(CFLAGS) $(IDRIS_RTS_CFLAGS) $< -o $@
+
+$(OBJDIR)/rts/%.cpp.o: $(IDRIS_RTS_PATH)/%.cpp $(COMMON_DEPS) | $(OBJDIR)
+	@$(MKDIR) $(dir $@)
+	$(CXX) -MMD -c $(CPPFLAGS) $(CXXFLAGS) $(IDRIS_RTS_CFLAGS) $< -o $@
